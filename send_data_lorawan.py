41c43f896f1b812a851890bde932bcddcb4c613d
cksajil <sajilck@gmail.com>
2024-07-17 12:18:43 +0530

    try changing country

--- send_data_lorawan.py
+++ send_data_lorawan.py
@@ -67,7 +67,7 @@ app = bytearray(
 )
 
 # Initialize ThingsNetwork configuration
-ttn_config = TTN(devaddr, nwkey, app, country="AS")
+ttn_config = TTN(devaddr, nwkey, app, country="IN")
 # Initialize lora object
 lora = TinyLoRa(spi, cs, irq, rst, ttn_config, 0)
 
