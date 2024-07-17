cfa724d8e7268b1da0182157ec97c28ed2abfe8a
cksajil <sajilck@gmail.com>
2024-07-17 09:41:35 +0530

    update gitignore

---
+++ adafruit_tinylora/ttn_in.py
@@ -0,0 +1,17 @@
+"""
+`ttn_in`
+======================================================
+The Things Network Frequency Plans - IN865
+* Author(s): Your Name
+"""
+
+TTN_FREQS = {
+    0: (0xD8, 0xF9, 0x24),  # 865.0625 MHz
+    1: (0xD9, 0x06, 0x91),  # 865.4025 MHz
+    2: (0xD9, 0x14, 0x5E),  # 865.985 MHz
+    3: (0xD9, 0x1D, 0xC0),  # 866.550 MHz
+    4: (0xD9, 0x24, 0x00),  # 866.725 MHz
+    5: (0xD9, 0x2A, 0xE4),  # 867.100 MHz
+    6: (0xD9, 0x31, 0xA4),  # 867.300 MHz
+    7: (0xD9, 0x38, 0x64),  # 867.500 MHz
+}
